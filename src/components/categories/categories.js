af2aa1473918d6e09ca4d6ec3fb46c9355fd5078
Valentin Tayursky <valentin.tayursky@gmail.com>
2016-12-09 02:13:35 +0300

    add simple tree

--- src/components/categories/categories.js
+++ src/components/categories/categories.js
@@ -4,19 +4,27 @@ import Category from './category';
 import './categories.css';
 
 class CategoriesManager extends Component {
-    constructor(){
+    constructor() {
         super();
-        this.state = {list:['one','two','three']};
+        this.state = {
+            list: [
+                {data: 'one', children: [
+                    {data: 'four', children: [
+                        {data:'five', children: []}
+                    ]}
+                ]},
+                {data: 'two', children: []},
+                {data: 'three', children: []}]
+        };
     }
+
     render() {
         return (
             <div className="row">
                 <Jumbotron className="m-bottom-0">
-                    <ul>
-                        {this.state.list.map(function(listValue,i){
-                            return <Category key={i} value={listValue}/>;
-                        })}
-                    </ul>
+                    {this.state.list.map(function (listValue, i) {
+                        return <Category key={i} value={listValue}/>;
+                    })}
                 </Jumbotron>
             </div>
         )
--- src/components/categories/category.js
+++ src/components/categories/category.js
@@ -3,14 +3,31 @@ import {Button, ButtonGroup} from 'react-bootstrap';
 
 class Category extends Component {
     render() {
+        let sub;
+        if (this.props.value.children.length > 0) {
+            sub = this.props.value.children.map(function (listValue, i) {
+                return <Category key={i} value={listValue}/>;
+            });
+        }
         return (
-            <li className="clearfix">
-                <p>{this.props.value}
+            <div className="category clearfix">
+                <div className="wrapper clearfix">
+                {this.props.value.data}
                     <ButtonGroup className="pull-right">
-                        <Button bsStyle="primary">+</Button>
+                        <Button bsStyle="success" bsSize="small">
+                            <span className="glyphicon glyphicon-plus"></span></Button>
+                        <Button bsStyle="danger" bsSize="small">
+                            <span className="glyphicon glyphicon-trash"></span></Button>
                     </ButtonGroup>
-                </p>
-            </li>
+                    <ButtonGroup className="pull-left m-left-10">
+                        <Button bsStyle="primary" bsSize="small">
+                            <span className="glyphicon glyphicon-chevron-right"></span></Button>
+                    </ButtonGroup>
+                </div>
+                <div className="sub-wrapper">
+                {sub}
+                </div>
+            </div>
         )
     }
 }

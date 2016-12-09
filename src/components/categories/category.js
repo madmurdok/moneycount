d27b892ab1655af51048d614489e83f1e860137a
Valentin Tayursky <valentin.tayursky@gmail.com>
2016-12-10 01:50:10 +0300

    add click handler for tree collapse-show func

--- src/components/categories/category.js
+++ src/components/categories/category.js
@@ -2,30 +2,45 @@ import React, {Component} from 'react';
 import {Button, ButtonGroup} from 'react-bootstrap';
 
 class Category extends Component {
+    constructor(){
+        super();
+        this.state = {isOpen:false}
+    }
+    handleClick = (e) => {
+        e.preventDefault();
+        this.setState({isOpen: !this.state.isOpen});
+    };
+
     render() {
-        let sub;
-        if (this.props.value.children.length > 0) {
+        let sub, sign;
+        if (this.props.value.children.length > 0 && this.state.isOpen) {
             sub = this.props.value.children.map(function (listValue, i) {
                 return <Category key={i} value={listValue}/>;
             });
         }
+        if (this.state.isOpen) {
+            sign = <span className="glyphicon glyphicon-chevron-down"></span>
+        } else {
+            sign = <span className="glyphicon glyphicon-chevron-right"></span>
+        }
         return (
             <div className="category clearfix">
                 <div className="wrapper clearfix">
-                {this.props.value.data}
+                    {this.props.value.data}
+                    <ButtonGroup className="pull-left m-left-10">
+                        <Button bsStyle="primary" bsSize="small" onClick={this.handleClick}>
+                            {sign}
+                        </Button>
+                    </ButtonGroup>
                     <ButtonGroup className="pull-right">
                         <Button bsStyle="success" bsSize="small">
                             <span className="glyphicon glyphicon-plus"></span></Button>
                         <Button bsStyle="danger" bsSize="small">
                             <span className="glyphicon glyphicon-trash"></span></Button>
                     </ButtonGroup>
-                    <ButtonGroup className="pull-left m-left-10">
-                        <Button bsStyle="primary" bsSize="small">
-                            <span className="glyphicon glyphicon-chevron-right"></span></Button>
-                    </ButtonGroup>
                 </div>
                 <div className="sub-wrapper">
-                {sub}
+                    {sub}
                 </div>
             </div>
         )
